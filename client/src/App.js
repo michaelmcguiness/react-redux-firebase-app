6ab81a1e025e1feb1c31fb73e56fe120b535cfc9
michaelmcguiness <michaelmcguiness123@gmail.com>
2020-01-18 21:30:34 -0500

    functioning home and scream components

---
+++ client/src/App.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+
+//Components
+import Navbar from "./components/Navbar";
+
+// Pages
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: "#33c9dc",
+      main: "#00bcd4",
+      dark: "#008394",
+      contrastText: "#fff"
+    },
+    secondary: {
+      light: "#ff6333",
+      main: "#ff3d00",
+      dark: "#b22a00",
+      contrastText: "#fff"
+    }
+  },
+  typography: {
+    useNextVariants: true
+  }
+});
+
+function App() {
+  return (
+    <MuiThemeProvider theme={theme}>
+      <div className="App">
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
+  );
+}
+
+export default App;
---
+++ client/src/pages/Home.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+
+import Scream from "../components/Scream";
+
+const Home = () => {
+  const [state, setState] = useState({
+    screams: null
+  });
+  useEffect(() => {
+    axios
+      .get("/screams")
+      .then(res => {
+        setState({
+          screams: res.data
+        });
+        console.log(state);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item sm={8} xs={12}>
+        <p>Content...</p>
+        {state.screams ? (
+          state.screams.map(scream => (
+            <Scream key={scream.screamId} scream={scream} />
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </Grid>
+      <Grid item sm={4} xs={12}>
+        <p>Profile...</p>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Home;
