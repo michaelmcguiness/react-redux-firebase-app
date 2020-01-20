03f3cc0c277da83f19b62e725f62556405e18d88
michaelmcguiness <michaelmcguiness123@gmail.com>
2020-01-20 16:02:22 -0500

    fixed like misspelling bug

--- socialape-functions/functions/handlers/screams.js
+++ socialape-functions/functions/handlers/screams.js
@@ -13,7 +13,7 @@ exports.getAllScreams = (req, res) => {
           userHandle: doc.data().userHandle,
           createdAt: doc.data().createdAt,
           commentCount: doc.data().commentCount,
-          lkeCount: doc.data().likeCount,
+          likeCount: doc.data().likeCount,
           userImage: doc.data().userImage
         });
       });
